a0ed2ba5e462a020d18b0bfa83e1601667021dbc
Mark Conatser <mconatser@hiconversion.com>
2019-01-07 20:17:24 -0500

    fix css for minicart tests

--- view/frontend/web/js/hic/minicart-tests.js
+++ view/frontend/web/js/hic/minicart-tests.js
@@ -12,22 +12,22 @@ define([
 
         if (config) {
             if (config.paypalExpressState === 'test') {
-                var style = testCore.applyCss('.minicart-content-wrapper .paypal.checkout:not(.paypal-bml) {display:none;}');
+                var style = testCore.applyCss('#minicart-content-wrapper .paypal.checkout:not(.paypal-bml) {display:none;}');
                 testCore.addToApi('minicart', 'paypal', testCore.createRemoveFn(style));
             }
 
             if (config.paypalCreditState === 'test') {
-                var style = testCore.applyCss('.minicart-content-wrapper .paypal-bml.checkout {display:none;}');
+                var style = testCore.applyCss('#minicart-content-wrapper .paypal-bml.checkout {display:none;}');
                 testCore.addToApi('minicart', 'paypalCredit', testCore.createRemoveFn(style));
             }
 
             if (config.applePayState === 'test') {
-                var style = testCore.applyCss('.minicart-content-wrapper .applepay-minicart {display:none;}');
+                var style = testCore.applyCss('#minicart-content-wrapper .applepay-minicart {display:none;}');
                 testCore.addToApi('minicart', 'applePay', testCore.createRemoveFn(style));
             }
 
             if (config.googlePayState === 'test') {
-                var style = testCore.applyCss('.minicart-content-wrapper .googlepay-minicart-logo {display:none;}');
+                var style = testCore.applyCss('#minicart-content-wrapper .googlepay-minicart-logo {display:none;}');
                 testCore.addToApi('minicart', 'googlePay', testCore.createRemoveFn(style));
             }
         }
